f91b32b49dfcfaa24ae9c25351a9f4437f53f21b
Matthew Monroe <matt@alchemistmatt.com>
2016-01-17 11:48:18 -0800

    Changed column types when initializing a new Tickets or Ideas table. Description, adminreply, and userreply are now text. Date is now datetime instead of timestamp (which would auto-update when the row changed).

--- src/me/odium/simplehelptickets/DBConnection.java
+++ src/me/odium/simplehelptickets/DBConnection.java
@@ -53,9 +53,9 @@ public class DBConnection {
 		try {
 			stmt = con.createStatement();
 
-			String columnList = " (id INTEGER PRIMARY KEY, description varchar(128), date timestamp, "
+			String columnList = " (id INTEGER PRIMARY KEY, description text, date datetime, "
 					+ "uuid varchar(36), owner varchar(20), world varchar(30), x double(30,20), y double(30,20), z double(30,20), "
-					+ "p double(30,20), f double(30,20), adminreply varchar(128), userreply varchar(128), status varchar(16), "
+					+ "p double(30,20), f double(30,20), adminreply text, userreply text, status varchar(16), "
 					+ "admin varchar(30), expiration timestamp)";
 
 			String queryTickets = "CREATE TABLE IF NOT EXISTS " + Utilities.TICKET_TABLE_NAME + " " + columnList;
